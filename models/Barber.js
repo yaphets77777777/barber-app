2aaa904f10744e755612bfa2fba675f54f0775fe
yephets77777777 <yaphets0708@gmail.com>
2026-03-26 00:08:53 +0300

    Update barber photo

--- models/Barber.js
+++ models/Barber.js
@@ -23,7 +23,7 @@ barberSchema.statics.initDemo = async function() {
   if (count === 0) {
     return this.create({
       name: 'Ресул,погоняла ЮРА МАШИНКА',
-      avatar: 'https://via.placeholder.com/300x300?text=Barber',
+      avatar: '/img/barber.jpg',
       description: 'Профессиональный барбер с 5-летним опытом. Специализируюсь на мужских стрижках и бородах. Создаю стильные образы для современных мужчин.'
     });
   }
